a02aea82b00df9f225c6ec5f60c64bfc7b4bef2e
philipmoller <45422015+philipmoller@users.noreply.github.com>
2019-01-19 11:11:43 +0100

    Update color_detection.cpp

--- src/color_detection.cpp
+++ src/color_detection.cpp
@@ -13,6 +13,8 @@
 using namespace cv;
 
 static const std::string OPENCV_WINDOW = "Processed image";
+//static const std::string HSV_WINDOW = "HSV Image";
+//static const std::string Contour_WINDOW = "Contoured Image";
 
 int LowH = 1;
 int LowS = 0;
@@ -23,8 +25,11 @@ int HighV = 255;
 
 bool anomaly = false;
 
+int n = 0;
 double x_pose;
 double y_pose;
+double prev_x = 0;
+double prev_y = 0;
 char filename[80];
 
 class ImageConverter{
@@ -49,14 +54,14 @@ public:
  void imageCb(const sensor_msgs::ImageConstPtr& msg){
    cv_bridge::CvImagePtr cv_ptr;
    try{
-     cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::RGB8);
+     cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
    }
    catch (cv_bridge::Exception& e){
      ROS_ERROR("cv_bridge exception: %s", e.what());
      return;
    }
    cv::Mat hsv_img, mask_img;
-   cvtColor(cv_ptr->image, hsv_img, COLOR_RGB2HSV);
+   cvtColor(cv_ptr->image, hsv_img, COLOR_BGR2HSV);
    cv::inRange(hsv_img, cv::Scalar(LowH, LowS, LowV), cv::Scalar(HighH, HighS, HighV), mask_img);
 
    cv::erode(mask_img, mask_img, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
@@ -64,6 +69,10 @@ public:
 
    cv::dilate(mask_img, mask_img, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
    cv::erode(mask_img, mask_img, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+   //cv::namedWindow(HSV_WINDOW);
+   //cv::imshow(HSV_WINDOW, hsv_img);
+   //cv::waitKey(3);
 
    cv::imshow(OPENCV_WINDOW, mask_img);
    cv::waitKey(3);
@@ -73,25 +82,36 @@ public:
    cv::RNG rng(12345);
    cv::findContours(mask_img, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
 
-  cv::Mat drawing = Mat::zeros(mask_img.size(), CV_8UC3);
+  /*cv::Mat drawing = Mat::zeros(mask_img.size(), CV_8UC3);
    for( int i = 0; i< contours.size(); i++ ){
     Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255));
     drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
-  }
+  }*/
 
-  int largest_area = 9500; //Change value to adjust search size
+  //cv::namedWindow(Contour_WINDOW);
+  //cv::imshow(Contour_WINDOW, drawing);
+  //cv::waitKey(3);
+
+  int largest_area = 30000; //Change value to adjust search size
 
   for(int i = 0; i< contours.size(); i++ ){
     double area = contourArea(contours[i],false);
     if(area>largest_area){
-      sprintf(filename,"x:%d_y:%d.png", x_pose, y_pose);
-      cv::imwrite(filename,cv_ptr->image);
-      anomaly = true;
+      if(x_pose>prev_x+0.5 || x_pose<prev_x-0.5){
+        sprintf(filename,"Anomaly%d_x:%f_y:%f.png", n++, x_pose, y_pose);
+        cv::imwrite(filename,cv_ptr->image);
+        anomaly = true;
+      }
+      if(y_pose>prev_y+0.5 || y_pose<prev_y-0.5){
+        sprintf(filename,"Anomaly%d_x:%f_y:%f.png", n++, x_pose, y_pose);
+        cv::imwrite(filename,cv_ptr->image);
+        anomaly = true;
+      }
     }
   }
 
    image_pub_.publish(cv_ptr->toImageMsg());
- }
+  }
 };
 
 ros::Subscriber odom_sub_;
@@ -101,6 +121,8 @@ void clbk_asd(const nav_msgs::Odometry::ConstPtr& asd){
   y_pose = asd->pose.pose.position.y;
   if(anomaly){
     ROS_INFO("Anomaly detected at: x: %f, y: %f",x_pose, y_pose);
+    prev_x = x_pose;
+    prev_y = y_pose;
     anomaly = false;
     return;
   }
